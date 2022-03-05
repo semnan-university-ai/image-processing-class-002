536667a5eb0a915910ea603b929a781559e8e291
fatemeh456 <f.arghavan1375@gmail.com>
2022-03-05 14:58:37 +0330

    Update GPA.m

--- exercises/fatemeh456/GPA/GPA.m
+++ exercises/fatemeh456/GPA/GPA.m
@@ -6,29 +6,29 @@ studnetnumber = input("enter a number: ");
 if studnetnumber >= 0 && studnetnumber <= 20
     studnetnumber = studnetnumber * 5;
     if studnetnumber >= 97 && studnetnumber <= 100
-        ans = "A+"
+        ans = "4.0 Scale = '4.0' & Letter Grade = 'A+'"
     elseif studnetnumber >= 93 && studnetnumber <= 96
-        ans = "A"
+        ans = "4.0 Scale = '4.0' & Letter Grade = 'A'"
     elseif studnetnumber >= 90 && studnetnumber <= 92
-        ans = "A-"
+        ans = "4.0 Scale = '3.7' & Letter Grade = 'A-'"
     elseif studnetnumber >= 87 && studnetnumber <= 89
-        ans = "B+"
+        ans = "4.0 Scale = '3.3' & Letter Grade = 'B+'"
     elseif studnetnumber >= 83 && studnetnumber <= 86
-        ans = "B"
+        ans = "4.0 Scale = '3.0' & Letter Grade = 'B'"
     elseif studnetnumber >= 80 && studnetnumber <= 82
-        ans = "B-"
+        ans = "4.0 Scale = '2.7' & Letter Grade = 'B-'"
     elseif studnetnumber >= 77 && studnetnumber <= 79
-        ans = "C+"
+        ans = "4.0 Scale = '2.3' & Letter Grade = 'C+'"
     elseif studnetnumber >= 73 && studnetnumber <= 76
-        ans = "C"
+        ans = "4.0 Scale = '2.0' & Letter Grade = 'C'"
     elseif studnetnumber >= 70 && studnetnumber <= 72
-        ans = "C-"
+        ans = "4.0 Scale = '1.7' & Letter Grade = 'C-'"
     elseif studnetnumber >= 67 && studnetnumber <= 69
-        ans = "D+"
+        ans = "4.0 Scale = '1.3' & Letter Grade = 'D+'"
     elseif studnetnumber >= 65 && studnetnumber <= 66
-        ans = "D"
+        ans = "4.0 Scale = '1.0' & Letter Grade = 'D'"
     elseif studnetnumber < 65
-        ans = "E/F"
+        ans = "4.0 Scale = '0.0' & Letter Grade = 'E/F'"
     end
 else
     ans = "Out of range"
