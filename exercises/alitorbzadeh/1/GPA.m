f42ce68cc3914495c1ca2b80c6ca663623cdea76
Ali <95109502+alitorbzadeh@users.noreply.github.com>
2022-03-31 21:54:43 +0430

    Add files via upload

---
+++ exercises/alitorbzadeh/1/GPA.m
@@ -0,0 +1,26 @@
+%%    ali torabzadeh        
+%%%   digital image processing
+%%%%  Dr Yaghmaie
+%%%%% Mr Shokri
+%%%%%% first task
+
+clc;
+close all;
+close all;
+
+nomre=input("lotfan nomre khod ra vared namaeed=  ");
+
+
+if nomre<=20 && nomre>=16
+    disp("your grade is= A ")
+elseif nomre<=20 && nomre>=16
+    disp("your grade is= B ")
+elseif nomre<=15.99 && nomre>=14
+    disp("your grade is= C ")
+elseif nomre<=13.99 && nomre>=12
+    disp("your grade is= D ")
+elseif nomre<=11.99 && nomre>=10
+    disp("your grade is= E ")
+elseif nomre<=9.99
+    disp("your grade is= F ")    
+end
