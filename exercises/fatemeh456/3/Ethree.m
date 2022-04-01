925b71a98f381d3e92c620de115dd468279f1845
fatemeh456 <f.arghavan1375@gmail.com>
2022-04-01 17:20:31 +0430

    3

--- exercises/fatemeh456/3/Ethree.m
+++ exercises/fatemeh456/3/Ethree.m
@@ -3,13 +3,11 @@ clear;
 close all;
 img = imread('spring.png') ;
 img = rgb2gray(img) ;
-L = 50 ;   % length of the box 
-B = 50 ;   % breadth of the box 
-R = [0 0 ; L 0 ; L B ; 0 B] ;
-% random boxes at locations 
-N = 1 ;   % number of boxes
-idx = randsample(1:numel(img),N) ;  % ten boxes
-% get center 
+L = 50 ;   % length
+B = 50 ;   % breadth 
+R = [0 0 ; L 0 ; L B ; 0 B] ; % random Square at locations 
+N = 1 ;   % number of Squares
+idx = randsample(1:numel(img),N) ; 
 [idx,idy] = ind2sub(size(img),idx) ;
 % draw coordinates
 imshow(img)
