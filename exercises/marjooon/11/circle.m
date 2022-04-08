5507285ff4163cf589ba5b897ebb332a1ba5962c
Marjan <100559372+Marjooon@users.noreply.github.com>
2022-04-09 00:35:07 +0430

    Add files via upload

---
+++ exercises/marjooon/11/circle.m
@@ -0,0 +1,16 @@
+clc;
+clear;
+close all;
+img = imread('benchmark/lena.png');
+R = 70;
+img=rgb2gray(img);
+figure,imshow(img);
+x0 = size(img,2)/2;
+y0 = size(img,1)/2;
+hold on
+axis on
+Theta = 0:0.01:2*pi;
+x = x0 + cos(Theta)' * R;
+y = y0 + sin(Theta)' * R;
+plot(x0,y0,'r*')
+plot(x,y,'r','LineWidth',3)
