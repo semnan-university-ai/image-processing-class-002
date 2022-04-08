81cdc3f068267e3def07f13268e75ab208b0548a
Marjan <100559372+Marjooon@users.noreply.github.com>
2022-04-08 23:55:32 +0430

    Add files via upload

---
+++ exercises/marjooon/3/square.m
@@ -0,0 +1,37 @@
+clear;
+close all;
+clc;
+
+img= imread('lena.png');
+img=rgb2gray(img);
+
+size=size(img);
+Width = size(1,1);
+Height = size(1,2);
+
+figure,imshow(img)
+hold on
+axis on
+
+xrandom=randi(Width);
+yrandom=randi(Height);
+
+x=[50 , 50 , 0, 0 ];
+y=[50 , 0 , 0 , 50];
+
+
+if xrandom<(Height-50) && yrandom<(Width-50)
+    plot( x+xrandom, y+yrandom , 'b', 'MarkerSize', 10, 'LineWidth', 1);
+    fill(x+xrandom,y+yrandom , 'w')
+else
+    while xrandom>(Height-50) || yrandom>(Width-50)
+     xrandom=randi(Height-50);
+     yrandom=randi(Width-50);
+     plot( x+xrandom, y+yrandom , 'b', 'MarkerSize', 10, 'LineWidth', 1);
+     fill(x+xrandom,y+yrandom , 'w')
+     break;
+     
+end
+  
+end
+
