a38edccdac362d39b4fa5ad94f6e72c84821ce27
mobina-t77 <mobina.talebian@yahoo.com>
2022-04-08 13:06:16 +0430

    Add files via upload

---
+++ exercises/mobina-t77/7/SEVEN.m
@@ -0,0 +1,21 @@
+
+
+clc;
+clear;
+close all;
+
+
+
+EydImageDB{1}=imread('1.jpg');
+EydImageDB{2}=imread('2.jpg');
+EydImageDB{3}=imread('3.jpg');
+EydImageDB{4}=imread('4.jpg');
+EydImageDB{5}=imread('5.jpg');
+EydImageDB{6}=imread('6.jpg');
+EydImageDB{7}=imread('7.jpg');
+EydImageDB{8}=imread('8.jpg');
+EydImageDB{9}=imread('9.jpg');
+EydImageDB{10}=imread('10.jpg');
+EydImageDB{11}=imread('11.jpg');
+EydImageDB{12}=imread('12.jpg');
+
