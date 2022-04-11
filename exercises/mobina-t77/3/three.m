f7654a7a1643c37ca1121184747f760849effa0e
mobina-t77 <mobina.talebian@yahoo.com>
2022-04-12 00:15:45 +0430

    Add files via upload

--- exercises/mobina-t77/3/three.m
+++ exercises/mobina-t77/3/three.m
@@ -2,12 +2,13 @@
 clc;
 clear;
 close all;
+
 img=imread('peppers.png');
 imshow(img);
-
+a=size(img)
 axis on;
 hold on;
 impixelinfo(); % Show x, y, RGB as you mouse around over image.
-xLeft = randi(255)  % integer random number between 1 and 255; % Wherever you want.
-yTop = randi(255)
+xLeft = randi(462)  % integer random number between 1 and (512-50); .
+yTop= randi(462)
 rectangle('Position', [xLeft, yTop, 50, 50], 'EdgeColor', 'w', 'LineWidth', 2);
