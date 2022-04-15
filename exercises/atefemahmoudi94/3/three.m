e5915ad2985c99632913206064cc4177c3ebb0d7
atefemahmoudi94 <95427624+atefemahmoudi94@users.noreply.github.com>
2022-04-16 00:14:26 +0430

    Update three.m

--- exercises/atefemahmoudi94/3/three.m
+++ exercises/atefemahmoudi94/3/three.m
@@ -2,19 +2,22 @@ clc;
 close all;
 clear;
 
-img = imread("baboon.png");
+img = imread("monarch.png");
 img = rgb2gray(img);
-figure, imshow(img);                                                                                 img_siz = size(img);
+figure, imshow(img);
+img_siz = size(img);
 sw = img_siz(1,1);
 sh = img_siz(1,2);
 
 figure,imshow(img);
 hold on;
-axis on;                                                                                                                      random_x = randi(sw);
+axis on;
+random_x = randi(sw);
 random_y = randi(sh);
 
 s_x = [50 , 50 , 0, 0 ];
-s_y = [50 , 0 , 0 , 50];                                                                                                 if random_x<(sh - 50) && random_y<(sw - 50)
+s_y = [50 , 0 , 0 , 50];
+if random_x<(sh - 50) && random_y<(sw - 50)
     plot(s_x + random_x, s_y + random_y , 'b', 'MarkerSize', 10, 'LineWidth', 1);
     fill(s_x + random_x, s_y + random_y , 'w');
 else
