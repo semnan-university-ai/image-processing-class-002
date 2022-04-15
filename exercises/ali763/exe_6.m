76a63d07424f75106b89e5d6bc8d42f7b07f8275
ali763 <56272972+ali763@users.noreply.github.com>
2022-04-15 23:56:51 +0430

    Update exe_6.m

--- exercises/ali763/exe_6.m
+++ exercises/ali763/exe_6.m
@@ -3,7 +3,7 @@ image = imread('cameraman.tif');
 image = double(image);
  for i=1:254
      for j=1:254
-       image(i+1,j+1) =sqrt(sum(image(i,j+1)+image(i+1,j)+image(i+1,j+2)+image(i+2,j+1)));
+       image(i+1,j+1) =sqrt(image(i,j+1)+image(i+1,j)+image(i+1,j+2)+image(i+2,j+1));
      end
  end
 imshow(uint8(image))
