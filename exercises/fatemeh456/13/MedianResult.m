9ce96a3a410bc9b8f395ddc0660750fe15fa8823
fatemeh456 <94125115+fatemeh456@users.noreply.github.com>
2022-04-24 00:36:34 +0430

    Add files via upload
    13

---
+++ exercises/fatemeh456/13/MedianResult.m
@@ -0,0 +1,18 @@
+clc;
+clear all;
+close all;
+
+%median
+t = tiledlayout('flow');
+nexttile;imshow(median('1.jpg'));title('median 1');
+nexttile;imshow(median('2.jpg'));title('median 2');
+nexttile;imshow(median('3.jpg'));title('median 3');
+nexttile;imshow(median('4.jpg'));title('median 4');
+nexttile;imshow(median('5.jpg'));title('median 5');
+nexttile;imshow(median('6.jpg'));title('median 6');
+nexttile;imshow(median('7.jpg'));title('median 7');
+nexttile;imshow(median('8.jpg'));title('median 8');
+nexttile;imshow(median('9.jpg'));title('median 9');
+nexttile;imshow(median('10.jpg'));title('median 10');
+nexttile;imshow(median('11.jpg'));title('median 11');
+nexttile;imshow(median('12.jpg'));title('median 12');
---
+++ exercises/fatemeh456/13/median.m
@@ -0,0 +1,16 @@
+function medianFilter = median(image)
+window_size = 7
+pad = (window_size - 1)/2;
+image = imread(image);
+image = rgb2gray(image);
+Result_image = image;
+[x y]=size(image);
+for i=1+pad:x-pad
+    for j=1+pad:y-pad
+        mat = image(i-pad:i+pad, j-pad:j+pad);
+        mat = sort(mat(:));
+        Result_image(i,j)= mat((end-1)/2);
+    end
+end
+figure;imshow(Result_image);
+end
