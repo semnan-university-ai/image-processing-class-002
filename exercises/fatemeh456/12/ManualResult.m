1fdabc05f0393e948575774d37cc30909121b00b
fatemeh456 <94125115+fatemeh456@users.noreply.github.com>
2022-04-23 20:07:52 +0430

    Add files via upload
    12

---
+++ exercises/fatemeh456/12/ManualResult.m
@@ -0,0 +1,17 @@
+clc;
+close all;
+clear;
+
+t = tiledlayout('flow');
+nexttile;histogrameq('1.jpg');title('Picture 1');
+nexttile;histogrameq('2.jpg');title('Picture 2');
+nexttile;histogrameq('3.jpg');title('Picture 3');
+nexttile;histogrameq('4.jpg');title('Picture 4');
+nexttile;histogrameq('5.jpg');title('Picture 5');
+nexttile;histogrameq('6.jpg');title('Picture 6');
+nexttile;histogrameq('7.jpg');title('Picture 7');
+nexttile;histogrameq('8.jpg');title('Picture 8');
+nexttile;histogrameq('9.jpg');title('Picture 9');
+nexttile;histogrameq('10.jpg');title('Picture 10');
+nexttile;histogrameq('11.jpg');title('Picture 11');
+nexttile;histogrameq('12.jpg');title('Picture 12');
