a189fa9553d4c082add0ed5f4a8219c8d7e53e3b
mahdieh yazdi <mahdiehyazdi801@gmail.com>
2022-04-30 01:09:12 +0430

    add EXC13

---
+++ exercises/mahdiehyazdi/EXC13/mean/EXC13-1.m
@@ -0,0 +1,23 @@
+clc;
+close all;
+clear;
+pkg load image;
+
+
+for n=1:12
+    img=rgb2gray(imread(strcat('./',int2str(n+8),'.jpg')));
+    h=fspecial('average',[7 7]);
+    mean_filter=imfilter(img,h);
+    figure(1);
+    subplot(3,4,n),imshow(img),title(n);
+    figure(2);
+    subplot(3,4,n),imshow(mean_filter),title(n);
+end
+
+
+
+  
+
+
+
+
