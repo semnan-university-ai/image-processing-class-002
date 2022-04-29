59dbbe4c449b355df3c6dcb2a0c98caa80a340fa
Marjan <100559372+Marjooon@users.noreply.github.com>
2022-04-30 01:55:44 +0430

    Add files via upload

---
+++ exercises/marjooon/12/HisHaftsin.m
@@ -0,0 +1,86 @@
+clc;
+close all;
+clear;
+
+img1 = rgb2gray(imread('1.jpeg'));
+figure , imshow(img1)
+figure, imhist(img1);
+figure, imshow(histeq(img1));
+figure, imhist(histeq(img1));
+
+
+img2 = rgb2gray(imread('2.jpeg'));
+figure , imshow(img2)
+figure, imhist(img2);
+figure, imshow(histeq(img2));
+figure, imhist(histeq(img2));
+
+
+img3 = rgb2gray(imread('3.jpeg'));
+figure , imshow(img3)
+figure, imhist(img3);
+figure, imshow(histeq(img3));
+figure, imhist(histeq(img3));
+
+
+img4 = rgb2gray(imread('4.jpeg'));
+figure , imshow(img4)
+figure, imhist(img4);
+figure, imshow(histeq(img4));
+figure, imhist(histeq(img4));
+
+
+img5 = rgb2gray(imread('5.jpeg'));
+figure , imshow(img5)
+figure, imhist(img5);
+figure, imshow(histeq(img5));
+figure, imhist(histeq(img5));
+
+
+img6 = rgb2gray(imread('6.jpeg'));
+figure , imshow(img6)
+figure, imhist(img6);
+figure, imshow(histeq(img6));
+figure, imhist(histeq(img6));
+
+
+img7 = rgb2gray(imread('7.jpeg'));
+figure , imshow(img7)
+figure, imhist(img7);
+figure, imshow(histeq(img7));
+figure, imhist(histeq(img7));
+
+
+img8 = rgb2gray(imread('8.jpeg'));
+figure , imshow(img8)
+figure, imhist(img8);
+figure, imshow(histeq(img8));
+figure, imhist(histeq(img8));
+
+
+img9 = rgb2gray(imread('9.jpeg'));
+figure , imshow(img9)
+figure, imhist(img9);
+figure, imshow(histeq(img9));
+figure, imhist(histeq(img9));
+
+
+img10 = rgb2gray(imread('10.jpeg'));
+figure , imshow(img10)
+figure, imhist(img10);
+figure, imshow(histeq(img10));
+figure, imhist(histeq(img10));
+
+
+img11 = rgb2gray(imread('11.jpeg'));
+figure , imshow(img11)
+figure, imhist(img11);
+figure, imshow(histeq(img11));
+figure, imhist(histeq(img11));
+
+
+img12 = rgb2gray(imread('12.jpeg'));
+figure , imshow(img12)
+figure, imhist(img12);
+figure, imshow(histeq(img12));
+figure, imhist(histeq(img12));
