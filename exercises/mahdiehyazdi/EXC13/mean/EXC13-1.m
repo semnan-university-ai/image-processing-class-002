a608f86a58996f5448f213d49fac7833249919ab
mahdieh yazdi <57790098+mahdiehyazdi@users.noreply.github.com>
2022-04-30 01:51:48 +0430

    Update EXC13-1.m

--- exercises/mahdiehyazdi/EXC13/mean/EXC13-1.m
+++ exercises/mahdiehyazdi/EXC13/mean/EXC13-1.m
@@ -5,8 +5,8 @@ clear;
 
 for n=1:12
     img=rgb2gray(imread(strcat('./',int2str(n+8),'.jpg')));
-    h=fspecial('average',[7 7]);
-    mean_filter=imfilter(img,h);
+    k=fspecial('average',[7 7]);
+    mean_filter=imfilter(img,k);
     figure(1);
     subplot(3,4,n),imshow(img),title(n);
     figure(2);
