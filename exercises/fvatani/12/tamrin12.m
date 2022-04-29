8152148e446e76e176b4563f2c0d18f84c64b7b5
Fvatani <31390787+Fvatani@users.noreply.github.com>
2022-04-29 11:50:13 +0430

    Update tamrin12.m

--- exercises/fvatani/12/tamrin12.m
+++ exercises/fvatani/12/tamrin12.m
@@ -8,40 +8,41 @@ I=dir(fullfile(folder,'*.jpg'));
 for k=1:numel(I)
     filename=fullfile(folder,I(k).name);
     image=imread(filename);
-     
+
+    image=rgb2gray(image); 
     finalResult = uint8(zeros(size(image)));
     frequncy = zeros(256,1);
-    pdf = zeros(256,1);
     cdf = zeros(256,1);
-    cummlative = zeros(256,1);
     result = zeros(256,1);
     sum =0;
 
     for x = 1:size(image,1)
         for y = 1:size(image,2)
-            val = image(x,y);
-            frequncy(val+1) = frequncy(val+1)+1;
-            pdf(val+1) = frequncy(val+1)/(size(image,1)*size(image,2));
+            value = image(x,y);
+            frequncy(value+1) = frequncy(value+1)+1;
         end
     end
-
-    for x = 1:size(pdf)
+    
+    
+    for x = 1:size(cdf)
         sum =sum +frequncy(x);
-        cummlative(x) = sum;
-        cdf(x) = cummlative(x)/ (size(image,1)*size(image,2));
-        result(x) = round(cdf(x) * 255);
+        cdf(x) = sum;
     end
 
-
+    
+    for x = 1:size(result)
+        result(x) =round((((cdf(x)- min(cdf))/(max(cdf)-min(cdf))))*255);
+    end
+        
+ 
     for x = 1:size(image,1)
         for y = 1:1:size(image,2)
             finalResult(x,y) = result(image(x,y) + 1);
         end
     end
     
-    subplot(1,2,1),imshow(image),title('original image -' + string(k));
+    figure(k);                 
+    subplot(1,2,1),imshow(image),title('original image - ' + string(k));
     subplot(1,2,2),imshow(finalResult),title('histogram equalization - ' + string(k));
 
 end
-
-                 
