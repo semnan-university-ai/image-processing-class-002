37d002e689479cefd2157563586f4f99c20bc456
fatemeh456 <94125115+fatemeh456@users.noreply.github.com>
2022-05-01 19:03:50 +0430

    Update Esixteen.m

--- exercises/fatemeh456/16/Esixteen.m
+++ exercises/fatemeh456/16/Esixteen.m
@@ -1,11 +1,3 @@
-clc;
-close all;
-clear;
-
-img = imread("fruit.jfif ");
-figure, imshow(img);
-[x,y,~] = size(img);
-
 for i = 1 : x
     for j = 1 : y
         for k = 1 : 3
@@ -18,43 +10,36 @@ for i = 1 : x
                 end
 
             elseif img(i, j, k) < 102 && img(i, j, k) >= 51
-                if img(i, j, k) < 102/2
+                if img(i, j, k) < (102-51)/2
                     img(i, j, k) = 51 ;
                 else
                     img(i, j, k) = 102 ;
                 end
 
             elseif img(i, j, k) < 153 && img(i, j, k) >= 102
-                if img(i, j, k) < 153/2
+                if img(i, j, k) < (153-102)/2
                     img(i, j, k) = 102 ;
                 else
                     img(i, j, k) = 153 ;
                 end
                 
             elseif img(i, j, k) < 204 && img(i, j, k) >= 153 
-                if img(i, j, k) < 204/2
+                if img(i, j, k) < (204-153)/2
                     img(i, j, k) = 153 ;
                 else
                     img(i, j, k) = 204 ;
                 end
 
-            elseif img(i, j, k) > 153 && img(i, j, k) >= 204
-                if img(i, j, k) < 153/2
+            elseif img(i, j, k) < 255 && img(i, j, k) >= 204
+                if img(i, j, k) < (255-204)/2
                     img(i, j, k) = 204 ;
-                else
-                    img(i, j, k) = 153 ;
-                end
-
-            else
-                if img(i, j, k) < 255/2
-                    img(i, j, k) = 153 ;
                 else
                     img(i, j, k) = 255 ;
                 end
+
+            else
+                img(i, j, k) = 255 ;
             end
         end
     end
 end
-
-figure, imshow(img);
-imwrite(img1,'MyImage.jpg')
