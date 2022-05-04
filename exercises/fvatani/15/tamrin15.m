588dda9699cc94e4ac05ba588c3dafb02a7be0a1
Fvatani <31390787+Fvatani@users.noreply.github.com>
2022-05-04 14:34:38 +0430

    Update tamrin15.m

--- exercises/fvatani/15/tamrin15.m
+++ exercises/fvatani/15/tamrin15.m
@@ -2,14 +2,14 @@ clc;
 clear all;
 close all;
 
-img= imread('pears.png');
-gray_image = uint8(zeros(size(img,1),size(img,2)));
+image= imread('pears.png');
+gray_image = uint8(zeros(size(image,1),size(image,2)));
 
-for i=1 : size(img,1)
-    for j=1 : size(img,2)
-         R = img(i,j,1);
-         G = img(i,j,2);
-         B = img(i,j,3);
+for i=1 : size(image,1)
+    for j=1 : size(image,2)
+         R = image(i,j,1);
+         G = image(i,j,2);
+         B = image(i,j,3);
          gray_image(i,j)=(R*0.299)+(G*0.587)+(B*0.114);
          R=0;G=0;B=0;
     end
