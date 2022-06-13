d4318a671f6ad73d2edab40408826abb1c016a44
EvE <93923361+EvEAskari@users.noreply.github.com>
2022-06-13 08:06:04 +0430

    Create Train5000.m

---
+++ project/eveaskari/Code/Train5000.m
@@ -0,0 +1,27 @@
+function [test_matrix, RGB, maxRGB, minRGB] = Train1000()
+
+root = '../train/5000/';
+path = '*.jpg';
+money = dir([root path]);
+lenm = max(size(money));
+
+test_matrix = zeros(lenm , 3);
+
+for i = 1: lenm
+    
+    img = imread([root money(i).name]);
+    
+    [Rm ,Gm ,Bm] = MeanRGB(img);
+    
+    test_matrix(i,1) = Rm;
+    test_matrix(i,2) = Gm;
+    test_matrix(i,3) = Bm;
+    
+    RGB = round(mean(test_matrix, 1));
+    
+    maxRGB = max(test_matrix);
+    minRGB = min(test_matrix);
+        
+end 
+
+end
