e0d7166b6ccba242a89694e304180d328a312f88
mobina-t77 <mobina.talebian@yahoo.com>
2022-06-15 23:11:00 +0430

    second tamrin

---
+++ exercises/mobina-t77/12/twelve.m
@@ -0,0 +1,66 @@
+clc;
+close all;
+clear;
+
+% % predefined function
+
+% img1=imread('boat.png');
+% figure,imhist(img1)
+% figure,imshow(img1)
+% new_img1=histeq(img1)
+% figure,imshow(new_img1)
+% figure,imhist(new_img1)
+
+
+%implementation
+
+path='C:\Users\Mobina\Desktop\term 4\tamrin -hal-tamrin\12';
+folder=dir(fullfile(path,'*.jpg'));
+
+for num=1
+    picture=fullfile(path,folder(num).name);
+    img1=imread(picture);
+    img2 = rgb2gray(img1);
+    img3 = img2;
+    [row,column] = size(img2);
+    NumPixel = zeros(1,256);
+    for i = 1:row
+        for j = 1:column
+        NumPixel( img2(i,j) + 1 ) = NumPixel( img2(i,j) + 1 ) + 1;
+        end
+    end
+
+    ProbPixel = zeros(1,256);
+    for i = 1:256
+        ProbPixel(i) = NumPixel(i) / (row * column * 1.0);
+    end
+
+    Cumupixel = zeros(1,256);
+    for i = 1:256
+        if i == 1
+            Cumupixel(i) = ProbPixel(i);
+        else
+            Cumupixel(i) = Cumupixel(i - 1) + ProbPixel(i);
+        end
+    end
+
+    Map = zeros(1,256);
+    for i = 1:256
+        Map(i) = uint8(255 * Cumupixel(i) + 0.5);
+    end
+    
+    for i = 1:row
+        for j = 1:column
+            img2(i,j) = Map(img2(i,j) + 1);
+         end
+    end
+    
+
+figure,imshow(img1);title('color-ful image');
+figure,imhist(img1);title('hist of color-ful image');
+figure,imshow(img3);title('gray_scale image');
+figure,imhist(img3);title('hist of gray_scale image');
+figure,imshow(img2);title('equalized image');
+figure,imhist(img2);title('hist of equalized image');
+
+end
---
+++ exercises/mobina-t77/13/thirteen.m
@@ -0,0 +1,83 @@
+
+clc;
+close all;
+clear;
+
+path='C:\Users\Mobina\Desktop\term 4\tamrin -hal-tamrin\13';
+folder=dir(fullfile(path,'*.jpg'));
+
+for num=1:12
+    picture=fullfile(path,folder(num).name);
+    img1=imread(picture);
+
+% %   test with peppers
+%  img1=imread('peppers.png');
+
+img2 = rgb2gray(img1);
+
+
+img3 = padarray(img2,[1  1],'symmetric');
+img4 = padarray(img3,[1  1],'symmetric');
+img5 = padarray(img4,[1  1],'symmetric');
+
+[row,column] = size(img5);
+
+% mean7*7
+
+img6=zeros;
+
+for i=4:(row-3)
+    for j=4:(column-3)
+        total = double(img5(i,j)) + ...
+                double(img5(i,j+1)) + double(img5(i,j+2)) + double(img5(i,j+3))+ double(img5(i,j-1)) + double(img5(i,j-2)) + double(img5(i,j-3)) + ...
+                double(img5(i+1,j)) + double(img5(i+2,j)) + double(img5(i+3,j))+ double(img5(i-1,j)) + double(img5(i-2,j)) + double(img5(i-3,j)) + ...
+                double(img5(i+1,j+1)) + double(img5(i+1,j+2)) + double(img5(i+1,j+3))+ double(img5(i+1,j-1)) + double(img5(i+1,j-2)) + double(img5(i+1,j-3)) + ...
+                double(img5(i-1,j+1)) + double(img5(i-1,j+2)) + double(img5(i-1,j+3))+ double(img5(i-1,j-1)) + double(img5(i-1,j-2)) + double(img5(i-1,j-3)) + ...
+                double(img5(i+2,j+1)) + double(img5(i+2,j+2)) + double(img5(i+2,j+3))+ double(img5(i+2,j-1)) + double(img5(i+2,j-2)) + double(img5(i+2,j-3)) + ...
+                double(img5(i-2,j+1)) + double(img5(i-2,j+2)) + double(img5(i-2,j+3))+ double(img5(i-2,j-1)) + double(img5(i-2,j-2)) + double(img5(i-2,j-3)) + ...
+                double(img5(i+3,j+1)) + double(img5(i+3,j+2)) + double(img5(i+3,j+3))+ double(img5(i+3,j-1)) + double(img5(i+3,j-2)) + double(img5(i+3,j-3)) + ...
+                double(img5(i-3,j+1)) + double(img5(i-3,j+2)) + double(img5(i-3,j+3))+ double(img5(i-3,j-1)) + double(img5(i-3,j-2)) + double(img5(i-3,j-3)) ;
+            
+                img6(i-3,j-3)= double(total/49) ;
+                total = 0;
+    end
+end
+
+result1=mat2gray(img6);
+
+figure, imshow(img1);title('orginal color image');
+figure, imshow(img2);title(' gray-scale image  ');
+
+figure, imshow(result1);title('mean filtered image');
+
+
+% median 7*7
+
+img7 = zeros;
+
+for i=4:(row-3)
+    for j=4:(column-3)
+       list_total = [double(img5(i,j)) , ...
+                double(img5(i,j+1)) , double(img5(i,j+2)) , double(img5(i,j+3)) , double(img5(i,j-1)) , double(img5(i,j-2)) , double(img5(i,j-3)) , ...
+                double(img5(i+1,j)) , double(img5(i+2,j)) , double(img5(i+3,j)) , double(img5(i-1,j)) , double(img5(i-2,j)) , double(img5(i-3,j)) , ...
+                double(img5(i+1,j+1)) , double(img5(i+1,j+2)) , double(img5(i+1,j+3)) , double(img5(i+1,j-1)) , double(img5(i+1,j-2)) , double(img5(i+1,j-3)) , ...
+                double(img5(i-1,j+1)) , double(img5(i-1,j+2)) , double(img5(i-1,j+3)) , double(img5(i-1,j-1)) , double(img5(i-1,j-2)) , double(img5(i-1,j-3)) , ...
+                double(img5(i+2,j+1)) , double(img5(i+2,j+2)) , double(img5(i+2,j+3)) , double(img5(i+2,j-1)) , double(img5(i+2,j-2)) , double(img5(i+2,j-3)) , ...
+                double(img5(i-2,j+1)) , double(img5(i-2,j+2)) , double(img5(i-2,j+3)) , double(img5(i-2,j-1)) , double(img5(i-2,j-2)) , double(img5(i-2,j-3)) , ...
+                double(img5(i+3,j+1)) , double(img5(i+3,j+2)) , double(img5(i+3,j+3)) , double(img5(i+3,j-1)) , double(img5(i+3,j-2)) , double(img5(i+3,j-3)) , ...
+                double(img5(i-3,j+1)) , double(img5(i-3,j+2)) , double(img5(i-3,j+3)) , double(img5(i-3,j-1)) , double(img5(i-3,j-2)) , double(img5(i-3,j-3))] ;
+            
+                   sorted_list_total = sort(list_total);
+                   median = sorted_list_total(25);
+                   img7(i,j)= double(median);
+                   sorted_list_total=[0];
+     end
+end
+    
+
+end
+
+result2=mat2gray(img7);
+
+figure, imshow(result2);title('median filtered image');
+    
