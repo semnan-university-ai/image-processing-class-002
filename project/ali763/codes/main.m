9f8bd2a031dd21b60abc91fa842e5ed050c33285
ali763 <56272972+ali763@users.noreply.github.com>
2022-06-17 15:09:17 +0430

    Create main.m

---
+++ project/ali763/codes/main.m
@@ -0,0 +1,49 @@
+clear,clc,close all
+test_money = imread('/Users/Ali/Desktop/image project/data/50000-b.jpeg');
+[a,b] = size(test_money);
+for i=1:14
+add='/Users/Ali/Desktop/image project/data';
+images=dir(fullfile(add,'*.jpeg'));
+pet=numel(images(i));
+image=images(i).name
+raw=imread(fullfile(add,image));
+resize_all = imresize(raw,[a,b/3]);
+[test_R,test_G,test_B]=Prepare(test_money);
+[source_R,source_G,source_B]=Prepare(resize_all);
+for j=1:a
+for k=1:(b/3)
+out= manhatan_distance(test_R(j,k),test_G(j,k),test_B(j,k),source_R(j,k),source_G(j,k),source_B(j,k));
+val_all(i)=mean(mean(out));
+end
+end
+end
+val=min(val_all);
+if val==val_all(1)
+     disp('the money is 1000 tomans');
+elseif val==val_all(2)
+     disp('the money is 1000 tomans');
+elseif val==val_all(5)
+     disp('the money is new_1000 tomans');
+elseif val==val_all(6)
+     disp('the money is new_1000 tomans');
+elseif val==val_all(7)
+    disp('the money is 2000 tomans');
+elseif val==val_all(8)
+     disp('the money is 2000 tomans');
+elseif val==val_all(9)
+  disp('the money is 5000 tomans');
+elseif val==val_all(10)
+     f disp('the money is 5000 tomans');
+elseif val==val_all(3)
+     disp('the money is 10000 tomans');
+elseif val==val_all(4)
+     disp('the money is 10000 tomans');
+elseif val==val_all(11)
+   disp('the money is 50000 tomans');
+elseif val==val_all(12)
+    disp('the money is 50000 tomans');
+elseif val==val_all(13)
+        disp('the money is new_50000 tomans');
+elseif val==val_all(14)
+     disp('the money is new_50000 tomans');
+end
